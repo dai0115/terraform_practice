71e1805749f0537a78af0db7096e3ed69c5d59df
dai0115 <hateen0808@gmail.com>
2022-12-27 08:18:51 +0900

    added lister for https and redirect

--- network/alb.tf
+++ network/alb.tf
@@ -47,8 +47,7 @@ module "http_redirect_sg" {
   cider_blocks = [ "0.0.0.0/0" ]
 }
 
-# リスナーの作成
-# httpリスエストに対するレスポンス
+# httpリスナー
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_alb.alb_example.arn
   port = 80
@@ -59,6 +58,38 @@ resource "aws_lb_listener" "http" {
       content_type = "text/plain"
       message_body = "これは「http」です"
       status_code = 200
+    }
+  }
+}
+
+# httpsリスナー
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_alb.alb_example.arn
+  port = 443
+  protocol = "HTTPS"
+  certificate_arn = aws_acm_certificate.acm_example.arn
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは「https」です"
+      status_code = 200
+    }
+  }
+}
+
+# リダイレクト用リスナー
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_alb.alb_example.arn
+  port = 8080
+  protocol = "HTTP"
+  default_action {
+    type = "redirect"
+    redirect {
+      port = 443
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
