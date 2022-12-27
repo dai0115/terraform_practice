ea4a23ac43ef395415fe97ed93715d6e8ff17cf1
dai0115 <hateen0808@gmail.com>
2022-12-27 12:19:40 +0900

    modulize iam_policy

--- iam/iam_policy.tf
+++ iam/iam_policy.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "ec2_assume_role" {
       actions = ["sts:AssumeRole"]
       principals {
         type = "Service"
-        identifiers = [ "ec2.amazonaws.com" ]
+        identifiers = [ var.identifier ]
       }
     }
 }
--- iam/variables.tf
+++ iam/variables.tf
@@ -2,3 +2,8 @@ variable "name" {
   type = string
   default = "describe-regions-for-ec2"
 }
+
+variable "identifier" {
+  type = string
+  default = "ec2.amazonaws.com"
+}
