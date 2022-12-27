8b25fa3bf065f460a75d9d5e407d0746b2463e2c
dai0115 <hateen0808@gmail.com>
2022-12-27 12:19:57 +0900

    atacched necessary role to ecs

--- computing/ecs.tf
+++ computing/ecs.tf
@@ -11,6 +11,7 @@ resource "aws_ecs_cluster" "ecs_cluster" {
    network_mode = "awsvpc"
    requires_compatibilities = [ "FARGATE" ]
    container_definitions = file("./computing/container_definition.json")
+   execution_role_arn = module.ecs_task_execution_role.iam_role_arn # cloudwatch logsへのロギング用のロール
 }
 
 # ECSサービス
@@ -52,4 +53,29 @@ module "nginx_sg" {
     vpc_id = var.vpc_id
     port = 80
     cider_blocks = [ var.cidr_block ]
+}
+
+# ECSの基本的な実行権限に関するポリシーを参照
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+    arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# SSMパラメータストアとECSを結合するため実行ポリシーを定義
+data "aws_iam_policy_document" "ecs_task_execution" {
+    source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+    statement {
+      effect = "Allow"
+      actions = ["ssm:GetParameters", "kms:Decrypt"]
+      resources = ["*"]
+    }
+}
+
+# ECSと関連付けるIAMロール
+# タスク定義に関連付けて、そこから生成されたタスクも実行権限を得る
+module "ecs_task_execution_role" {
+  source = "../iam"
+  name = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy = data.aws_iam_policy_document.ecs_task_execution.json
 }
