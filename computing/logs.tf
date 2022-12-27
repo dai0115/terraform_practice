44ad8bdf9790b3b4bfcb3b75ed795abf9dc0f7b9
dai0115 <hateen0808@gmail.com>
2022-12-27 23:08:33 +0900

    added cloudwatch logs for ecs scheduled tasks

--- computing/logs.tf
+++ computing/logs.tf
@@ -1,4 +1,11 @@
+# webサーバのロギング用
 resource "aws_cloudwatch_log_group" "for_ecs" {
   name = "/ecs/logs"
+  retention_in_days = 30
+}
+
+# ECSタスクスケジュール用
+resource "aws_cloudwatch_log_group" "for_batch" {
+  name = "/ecs/scheduled-tasks/example"
   retention_in_days = 30
 }
