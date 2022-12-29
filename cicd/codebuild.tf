86d445b034471f3b3226587604db4d94b6d8435a
dai0115 <hateen0808@gmail.com>
2022-12-29 14:16:51 +0900

    created codebuild resource

---
+++ cicd/codebuild.tf
@@ -0,0 +1,55 @@
+# codebuildが各種操作をするためのポリシーを定義
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    effect = "Allow"
+    resources = ["*"]
+
+    actions = [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload",
+        "ecr:PutImage",
+        ]
+  }
+}
+
+# IAMロールの生成
+module "codebuild_role" {
+  source = "../iam"
+  name = "codebuild"
+  identifier = "codebuild.amazonaws.com"
+  policy = data.aws_iam_policy_document.codebuild.json
+}
+
+# codebuildの生成
+resource "aws_codebuild_project" "codebuild_project" {
+  name = "codebuild"
+  service_role = module.codebuild_role.iam_role_arn
+
+  source {
+    type = "CODEPIPELINE"
+  }
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type = "LINUX_CONTAINER"
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/standard:2.0"
+    privileged_mode = true
+  }
+}
